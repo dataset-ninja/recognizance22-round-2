f4cd478ac413cdf73799b6394588b32cdd13d40e
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-03 12:26:46 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -26,8 +26,8 @@ CATEGORY: Category = Category.Biology()
 
 CV_TASKS: List[CVTask] = [
     CVTask.InstanceSegmentation(),
+    CVTask.SemanticSegmentation(),
     CVTask.ObjectDetection(),
-    CVTask.InstanceSegmentation(),
     CVTask.Classification(),
 ]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
@@ -64,6 +64,7 @@ REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = ["Utkarsh Pandey"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://www.kaggle.com/kratosishere"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -117,6 +118,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
